133212a9535c8c83b6f71fdaee26eea996ea656c
danielnichols91 <danielnichols0912@gmail.com>
2023-03-25 16:54:51 -0400

    Working YouTube API Call

--- ChurchApp/screens/onlineService.js
+++ ChurchApp/screens/onlineService.js
@@ -1,47 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import YoutubePlayer from 'react-native-youtube-iframe';
 
-var videoID1; 
-var videoID2;
-getVideos = async () => {
-  try {
-    const response = await fetch('https://youtube.googleapis.com/youtube/v3/activities?part=snippet%2CcontentDetails&channelId=UCHo_NAei6lwlmC9HSFtO-Xw&maxResults=2&key=AIzaSyC2DjXLIDAeKi11KV4e3AIzuCUYQbd3vJs');
-    const json = await response.json();
-    //setData(json.items);
-    videoID1 = json.items[0].snippet.contentDetails.upload.videoId;
-    console.log(videoID1);
-    videoID2 = json.items[1].snippet.contentDetails.upload.videoId;
-    console.log(videoID2);
-  } catch (error) {
-    console.error(error);
-  } finally {
-    setLoading(false);
-  }
-}
 export default function OnlineService() {
+  const [videoID1, setVideoID1] = useState('');
+  const [videoID2, setVideoID2] = useState('');
+
+  const getVideos = async () => {
+    try {
+      const response = await fetch('https://youtube.googleapis.com/youtube/v3/activities?part=snippet%2CcontentDetails&channelId=UCHo_NAei6lwlmC9HSFtO-Xw&maxResults=3&key=AIzaSyC2DjXLIDAeKi11KV4e3AIzuCUYQbd3vJs');
+      const json = await response.json();
+      console.log(json.items[2].contentDetails);
+      setVideoID1(json.items[0].contentDetails.upload.videoId);
+      setVideoID2(json.items[2].contentDetails.upload.videoId);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
   return (
-    
     <View>
-      <YoutubePlayer
-        height={300}
-        play={false}
-        videoId={videoID1}
-      />
-      <YoutubePlayer
-        height={300}
-        play={false}
-        videoId={videoID2}
-      />
+      <YoutubePlayer height={300} play={false} videoId={videoID1} />
+      <YoutubePlayer height={300} play={false} videoId={videoID2} />
     </View>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#ffffff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
