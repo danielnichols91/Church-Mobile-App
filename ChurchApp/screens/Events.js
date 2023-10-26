9059d507deeb3b21b9d6c589dbde3e0bf7b24ecb
danielnichols91 <danielnichols0912@gmail.com>
2023-10-26 16:01:36 -0400

    Fixed Json Error

--- ChurchApp/screens/Events.js
+++ ChurchApp/screens/Events.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
 import BottomNavBar from '../bottomNavBar';
 
 export default function Events(props) {
     const url = "https://www.sjfirstumc.org/_functions/events";
     //const exampleURL = "https://api.github.com/users/hadley/orgs"; 
-    var newData;
     let [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
     const options= {
@@ -18,59 +17,23 @@ export default function Events(props) {
       const fetchData = () => { 
         fetch(url,options) 
           .then(response => response.json()) 
-          .then(json => setData(json)) 
+          .then(json => setData(json.items)) 
           .catch(error => console.log(error)) 
           .finally(() => setLoading(false));
       }; 
-    // async function getAllData(){
-        
-    // }
-    // useEffect(() => {
-    //     try {
-    //         // const response = fetch(url);
-    //         // const data = response.json();
-    //         // console.log(data);
-    //         const fetchData = fetch(url);
-    //         if (!fetchData.ok) {
-    //             const message = `An error has occured:`;
-    //             throw new Error(message);
-    //           }
-        
-    //           const data = fetchData.json();
-    //           console.log(data);
-    //             // .then((resp) => {
-    //             //     return resp.json();
-    //             // })
-    //             // .then((json) => {
-    //             //     console.log(json);
-    //             //     setData(json);
-    //             //     //newData = resp.body[0].location;
-    //             // })
-    //             // .catch((error) => console.error(error))
-    //             // .finally(() => setLoading(false)); 
-    //     } catch (error) {
-    //         console.log('response error')
-    //         console.log(error);
-    //     }
-       
-    // }, []);
-    //console.log(newData);
+      console.log(data);
+      const Event = ({title}) => (
+        <View style={styles.event}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      );
     return ( 
         <View style={styles.container}>
-        {/* {loading ? (
-            <Text>Loading...</Text>
-        ) : (
-            data.map((post) => {
-            return (
-                <View>
-                    <Text >Events</Text>
-                    <Text>{post.body}</Text>
-                </View>
-            );
-            })
-        )} */
-        <Text>{typeof(data)}</Text>
-        }
+            <FlatList
+              listData={data}
+              renderItem={({event}) => <Event title={event.title} />}
+              keyExtractor={event => event._id}
+            />
             <BottomNavBar/>
         </View>
         
@@ -81,4 +44,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
       },
+      event: {
+      },
+      title: {
+    },
 });
